4c3afc60487a03ce01e2755ef2b4fad7596aee74
kevin <kevinmibz6@gmail.com>
2022-06-20 10:52:28 +0300

    registering models to the addmin panel

--- door/admin.py
+++ door/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Neighbour)
+admin.site.register(Business)
+admin.site.register(Post)
+admin.site.register(Profile)
