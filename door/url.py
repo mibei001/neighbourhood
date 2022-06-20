a3b873904c74ce8bb303d0c91a48d8a2a6a48f15
kevin <kevinmibz6@gmail.com>
2022-06-20 11:28:12 +0300

    register profile view in the url file

--- door/url.py
+++ door/url.py
@@ -4,5 +4,5 @@ from .import views
 urlpatterns = [
     path('', views.home_page, name='home_page'),
     path("add_hood/", views.create_mtaa, name="add_hood"),
-
+    path("accounts/profile/", views.profile, name="profile")
 ]
