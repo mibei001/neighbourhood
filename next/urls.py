02d60693d976f6549de1d864fe16a9047a7bf180
kevin <kevinmibz6@gmail.com>
2022-06-20 14:08:50 +0300

    registering urls

--- next/urls.py
+++ next/urls.py
@@ -18,4 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include('door.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+
 ]
