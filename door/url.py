89edfc99685ae86a05cab9cdfe4b5df0d30457e5
kevin <kevinmibz6@gmail.com>
2022-06-20 14:04:19 +0300

    create business and logout view

--- door/url.py
+++ door/url.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.home_page, name='home_page'),
     path("add_hood/", views.create_mtaa, name="add_hood"),
     path("new_post/", views.new_post, name="new_post"),
-    path("accounts/profile/", views.profile, name="profile")
+    path("posts/", views.post, name="post"),
+    path("accounts/profile/", views.profile, name="profile"),
+    path("business/", views.new_business, name="business"),
+    path("logout/", views.logout, name="logout"),
+
 ]
