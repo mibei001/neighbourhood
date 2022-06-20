8c8d71faa28fee91b2dfbc9be4df30dcbbf18c81
kevin <kevinmibz6@gmail.com>
2022-06-20 11:05:20 +0300

    create home page view

--- door/views.py
+++ door/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import response
+from django.contrib.auth.decorators import login_required
+from django.http.response import Http404, HttpResponseRedirect
+from .models import Neighbour, Profile, Business, Post
+from django.contrib.auth.models import User
+from django.contrib.auth import logout as django_logout
+from .forms import *
 
 # Create your views here.
+
+
+@login_required(login_url='accounts/login/')
+def home_page(request):
+    try:
+        mtaa = Neighbour.objects.all()
+    except Exception as e:
+        raise Http404
+    return render(request, "index.html", {"mtaa": mtaa})
